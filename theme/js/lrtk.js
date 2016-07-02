671723fef5d8b27f9fa543e1c81010d596bd762f
gatieme <gatieme@163.com>
2016-07-02 08:04:04 +0800

    update

--- theme/js/lrtk.js
+++ theme/js/lrtk.js
@@ -26,27 +26,3 @@ jQuery(document).ready(function($){
 	});
 
 });
-
-<script type="text/javascript">
-<!--
-//分别是奇数行默认颜色,偶数行颜色,鼠标放上时奇偶行颜色
-var aBgColor = ["#FFFFFF","#f2faff","#FFFFCC","#FFFFCC"];
-//从前面iHead行开始变色，直到倒数iEnd行结束
-function addTableListener(o,iHead,iEnd)
-{
-o.style.cursor = "normal";
-iHead = iHead > o.rows.length?0:iHead;
-iEnd = iEnd > o.rows.length?0:iEnd;
-for (var i=iHead;i<o.rows.length-iEnd ;i++ )
-{
-o.rows[i].onmouseover = function(){setTrBgColor(this,true)}
-o.rows[i].onmouseout = function(){setTrBgColor(this,false)}
-}
-}
-function setTrBgColor(oTr,b)
-{
-oTr.rowIndex % 2 != 0 ? oTr.style.backgroundColor = b ? aBgColor[3] : aBgColor[1] : oTr.style.backgroundColor = b ? aBgColor[2] : aBgColor[0];
-}
-window.onload = function(){addTableListener(document.getElementById("tbColor"),0,0);}
-//-->
-</script>
