a5535c7351bb3dc5fa2a0e9009934a273f0b10f2
gatieme <gatieme@163.com>
2016-07-03 03:24:18 +0800

    update

--- gatieme.github/pelicanconf.py
+++ gatieme.github/pelicanconf.py
@@ -78,7 +78,8 @@ SOCIAL = (('GitHub', 'https://github.com/gatieme'),
 #RELATIVE_URLS = True
 
 # Theme
-THEME = './pelican-themes/zurb-F5-basic'
+#THEME = './pelican-themes/zurb-F5-basic'
+THEME = './pelican-themes/elegant'
 #THEME = './pelican-themes/tuxlite_tbs'
 #THEME = './pelican-themes/gum'
 #COVER_BG_COLOR = '#375152'
