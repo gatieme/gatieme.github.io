c9f57b91b5137b9915b52ee8e87735733d1353df
gatieme <gatieme@163.com>
2016-07-03 04:02:43 +0800

    update

--- gatieme.github/pelican-themes/tuxlite_tbs/static/js/lrtk.js
+++ gatieme.github/pelican-themes/tuxlite_tbs/static/js/lrtk.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($){
 	//hide or show the "back to top" link
 	$(window).scroll(function(){
 		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
+		if( $(this).scrollTop() > offset_opacity ) {
 			$back_to_top.addClass('cd-fade-out');
 		}
 	});
@@ -26,3 +26,26 @@ jQuery(document).ready(function($){
 	});
 
 });
+
+
+/* 当鼠标移到表格上是，当前一行背景变色 */
+$(document).ready(function(){
+		$(".data_list tr td").mouseover(function(){
+				$(this).parent().find("td").css("background-color","#d5f4fe");
+		});
+})
+/* 当鼠标在表格上移动时，离开的那一行背景恢复 */
+$(document).ready(function(){
+		$(".data_list tr td").mouseout(function(){
+				var bgc = $(this).parent().attr("bg");
+				$(this).parent().find("td").css("background-color",bgc);
+		});
+})
+
+$(document).ready(function(){
+		var color="#ffeab3"
+		$(".data_list tr:odd td").css("background-color",color);  //改变偶数行背景色
+		/* 把背景色保存到属性中 */
+		$(".data_list tr:odd").attr("bg",color);
+		$(".data_list tr:even").attr("bg","#fff");
+})
